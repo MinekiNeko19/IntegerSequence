dca7d8321904a7a6e389ec125f90d3b35d2b0f1d
Sarah Leong <sleong20@stuy.edu>
2020-12-04 17:49:59 -0500

    added and tested hasNext() and next()

--- Range.java
+++ Range.java
@@ -11,16 +11,33 @@ public class Range implements IntegerSequence{
     current = start;
   }
 
-  public void reset(){  }
-  public int length(){return 0;  }
+  public void reset() {
+    current = start;
+  }
+  public int length() {
+    return start-end;
+  }
   
   //When current is no longer a valid element in the range, it should return false. 
-  public boolean hasNext(){return false;  }
+  public boolean hasNext() {
+    if (current <= end) {
+        return true;
+    }
+    return false;
+  }
 
   //@throws NoSuchElementException when hasNext() is false.
   //This will return the current value, it will also increase current value by 1. 
   //e.g.  if current is 5. This will make current 6, and return 5. 
-  public int next(){return 0;    }
+  public int next() {
+    if (this.hasNext()) {
+        int pop = current;
+        current++;
+        return pop;
+    } else {
+        throw new NoSuchElementException();
+    }
+  }
   
   public static void main(String[] args) {
       Range a = new Range(20,40);
@@ -29,5 +46,15 @@ public class Range implements IntegerSequence{
       System.out.println(a.start);
       System.out.println(a.end);
       System.out.println(a.current);
+
+      // Testing next
+      try {
+        System.out.println(a.next());
+        for (int i = 0; i < 21; i++) {
+            System.out.println(a.next());
+        }
+      } catch (NoSuchElementException e) {
+        System.out.println("Out of range. Reset.");
+      }
     }
   }
