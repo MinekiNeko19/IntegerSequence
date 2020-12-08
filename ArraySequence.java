8dfc1cb1e65ff8a3cead48c32c76c6fa3a694d0b
Sarah Leong <sleong20@stuy.edu>
2020-12-08 12:49:17 -0500

    added and tested first constructor

--- ArraySequence.java
+++ ArraySequence.java
@@ -4,27 +4,54 @@ public class ArraySequence implements IntegerSequence{
     private int []data;
 
     /*Construct the sequence by copying values from the other array into the data array*/
-    public ArraySequence(int [] other){  }
+    public ArraySequence(int [] other){
+        data = new int[other.length];
+        for (int i = 0; i < other.length; i++) {
+            data[i] = other[i];
+        }
+        currentIndex = 0;
+    }
 
     //Postcondition: The otherseq will be reset.
     //This constructor will copy ALL values of the `otherseq` into the data array.
     public ArraySequence(IntegerSequence otherseq){  }
 
-    public static void main(String[] args) {
-    int[]nums = {1,3,5,0,-1,3,9};
-    IntegerSequence as = new ArraySequence(nums);
+    public boolean hasNext() {
+        if (data.length == currentIndex) return false;
+        return true;
+    }
 
-    System.out.println("ArraySequence(array):");
-    while(as.hasNext()){
-        System.out.print(as.next()+", ");
+    public int next() {
+        currentIndex++;
+        return data[currentIndex-1];
     }
-    IntegerSequence r = new Range(10,20);
-    IntegerSequence as = new ArraySequence(r);
-   
-    System.out.println("ArraySequence(seq):");
-    while(as.hasNext()){
-        System.out.print(as.next()+", ");
+
+    public int length() {
+        return 0;
     }
-    System.out.println(); 
+
+    public void reset() {
+
+    }
+
+    public static void main(String[] args) {
+        // Demo 1
+        int[]nums = {1,3,5,0,-1,3,9};
+        IntegerSequence as = new ArraySequence(nums);
+
+        System.out.println("ArraySequence(array):");
+        while(as.hasNext()){
+            System.out.print(as.next()+", ");
+        }
+
+        // Demo 2
+        // IntegerSequence r = new Range(10,20);
+        // IntegerSequence as = new ArraySequence(r);
+    
+        // System.out.println("ArraySequence(seq):");
+        // while(as.hasNext()){
+        //     System.out.print(as.next()+", ");
+        // }
+        // System.out.println();
     }
 }
