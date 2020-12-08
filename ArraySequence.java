9658f0c9876153171accbf0982dea99806526018
Sarah Leong <sleong20@stuy.edu>
2020-12-08 13:04:42 -0500

    added and tested otherseq constructor

--- ArraySequence.java
+++ ArraySequence.java
@@ -14,7 +14,17 @@ public class ArraySequence implements IntegerSequence{
 
     //Postcondition: The otherseq will be reset.
     //This constructor will copy ALL values of the `otherseq` into the data array.
-    public ArraySequence(IntegerSequence otherseq){  }
+    public ArraySequence(IntegerSequence otherseq){
+        data = new int[otherseq.length()+1];
+        int count = 0;
+        while (otherseq.hasNext()) {
+            data[count] = otherseq.next();
+            count++;
+        }
+        currentIndex = 0;
+        otherseq.reset();
+        
+    }
 
     public boolean hasNext() {
         if (data.length == currentIndex) return false;
@@ -27,31 +37,31 @@ public class ArraySequence implements IntegerSequence{
     }
 
     public int length() {
-        return 0;
+        return data.length;
     }
 
     public void reset() {
-
+        currentIndex = 0;
     }
 
     public static void main(String[] args) {
         // Demo 1
-        int[]nums = {1,3,5,0,-1,3,9};
-        IntegerSequence as = new ArraySequence(nums);
+        // int[]nums = {1,3,5,0,-1,3,9};
+        // IntegerSequence as = new ArraySequence(nums);
 
-        System.out.println("ArraySequence(array):");
-        while(as.hasNext()){
-            System.out.print(as.next()+", ");
-        }
-
-        // Demo 2
-        // IntegerSequence r = new Range(10,20);
-        // IntegerSequence as = new ArraySequence(r);
-    
-        // System.out.println("ArraySequence(seq):");
+        // System.out.println("ArraySequence(array):");
         // while(as.hasNext()){
         //     System.out.print(as.next()+", ");
         // }
-        // System.out.println();
+
+        // Demo 2
+        IntegerSequence r = new Range(10,20);
+        IntegerSequence as = new ArraySequence(r);
+    
+        System.out.println("ArraySequence(seq):");
+        while(as.hasNext()){
+            System.out.print(as.next()+", ");
+        }
+        System.out.println();
     }
 }
